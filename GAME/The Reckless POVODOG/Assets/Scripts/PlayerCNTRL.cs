d95c47e69dec347e909b361245c1878cc2aa320b
Aggrron <gsm10forhl2@yandex.ru>
2017-07-28 14:48:21 +0300

    Добавил пару фичей

--- GAME/The Reckless POVODOG/Assets/Scripts/PlayerCNTRL.cs
+++ GAME/The Reckless POVODOG/Assets/Scripts/PlayerCNTRL.cs
@@ -6,6 +6,7 @@ public class PlayerCNTRL : MonoBehaviour {
 	private Rigidbody2D rb;
 
 	public float speed = 5f;
+	public float jumpForce = 10f;
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody2D> ();	
@@ -16,5 +17,8 @@ public class PlayerCNTRL : MonoBehaviour {
 	void FixedUpdate(){
 		float move =  Input.GetAxis("Horizontal");
 		rb.velocity = new Vector2(move * speed * Time.deltaTime, rb.velocity.y);
+		if (Input.GetKeyDown (KeyCode.W)) {
+			rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+		}
 	}
 }
---
+++ GAME/The Reckless POVODOG/Assets/Scripts/Testing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour {
+
+	Vector2 position;
+	Vector3 position2;
+
+
+	void Start () {
+		
+		position = new Vector2 (2f, 6f);
+		Debug.Log ("Position x" + "   " + position.x);
+		Debug.Log (position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void FixedUpdate()
+	{
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			Debug.LogAssertion ("PRESSING A IS NOT ALLOWED");
+		}
+	}
+}
