a45477fea3b5eb94a15ac4d2077f9ee9239d4c90
Aggrron <gsm10forhl2@yandex.ru>
2017-08-29 00:52:02 +0300

    Ходьба

--- GAME/The Reckless POVODOG/Assets/Scripts/PlayerCNTRL.cs
+++ GAME/The Reckless POVODOG/Assets/Scripts/PlayerCNTRL.cs
@@ -7,11 +7,12 @@ public class PlayerCNTRL : MonoBehaviour {
 
 	public float speed = 5f;
 	public float jumpForce;
-	//private bool faceRight = true;
-	//private bool ladder = false;
+	private bool faceRight = true;
+	private Animator anim;
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody2D> ();	
+		anim = GetComponent <Animator> ();
 	}
 	void Update () {
 		
@@ -22,10 +23,18 @@ public class PlayerCNTRL : MonoBehaviour {
 		rb.velocity = new Vector2(move * speed * Time.deltaTime, rb.velocity.y);
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
-
-
-
 		}
+		if (move > 0 && !faceRight)
+			flip ();
+		else if (move < 0 && faceRight)
+			flip ();
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+	}
+	void flip()
+	{
+		faceRight = !faceRight;
+		transform.localScale = new Vector2 (transform.localScale.x * -1, transform.localScale.y);
+
 	}
 
 }
