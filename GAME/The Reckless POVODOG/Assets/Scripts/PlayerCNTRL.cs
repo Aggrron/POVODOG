18e13c56acbde706d20414468e090b427a0177de
Aggrron <gsm10forhl2@yandex.ru>
2017-09-05 18:33:58 +0300

    блабла

--- GAME/The Reckless POVODOG/Assets/Scripts/PlayerCNTRL.cs
+++ GAME/The Reckless POVODOG/Assets/Scripts/PlayerCNTRL.cs
@@ -9,32 +9,51 @@ public class PlayerCNTRL : MonoBehaviour {
 	public float jumpForce;
 	private bool faceRight = true;
 	private Animator anim;
+	public bool isGrounded;
+	Transform grounded;
+	public LayerMask layerMask;
+
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody2D> ();	
 		anim = GetComponent <Animator> ();
+		grounded = GameObject.Find(this.name + "/grounded").transform;
 	}
 	void Update () {
 		
 	}
 	void FixedUpdate(){
-		
+
+		isGrounded = Physics2D.Linecast (transform.position, grounded.position, layerMask);
 		float move =  Input.GetAxis("Horizontal");
 		rb.velocity = new Vector2(move * speed * Time.deltaTime, rb.velocity.y);
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+		if (Input.GetKeyDown (KeyCode.Space)&&isGrounded) {
+			Jump ();
+			anim.Play ("Jump", -1, 0f);
+		
 		}
 		if (move > 0 && !faceRight)
 			flip ();
 		else if (move < 0 && faceRight)
 			flip ();
 		anim.SetFloat ("Speed", Mathf.Abs (move));
+		anim.SetBool ("OnlyUp", isGrounded);
 	}
 	void flip()
 	{
 		faceRight = !faceRight;
 		transform.localScale = new Vector2 (transform.localScale.x * -1, transform.localScale.y);
-
+	}
+	void Jump()
+	{
+		rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
 	}
 
-}
+	}
+	
+	
+	
+	
+
+
+
