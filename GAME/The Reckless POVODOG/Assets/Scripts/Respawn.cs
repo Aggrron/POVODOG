e292cde5f600f525f85aca27b251a507ccc004e3
Aggrron <gsm10forhl2@yandex.ru>
2017-09-17 12:31:25 +0300

    yeesh

---
+++ GAME/The Reckless POVODOG/Assets/Scripts/Respawn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Respawn : MonoBehaviour {
+	void OnTriggerEnter2D (Collider2D obj){
+
+		if (obj.tag == "Player")
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+}
